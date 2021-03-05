035d8f46cea6a535b52c8492d0e81151903031c4
nithish <nithish98raja@gmail.com>
2021-03-05 17:41:42 +0530

    move configuration into separate file

--- index.py
+++ index.py
@@ -5,9 +5,15 @@
 
 # Dependencies
 from grid import Grid
+import json
+
+# Read configuration file
+file = open("config.json")
+# Load json data
+data = json.load(file)
 
 # Create object
-grid = Grid(10, 5, 0.5)
+grid = Grid(data["size"], data["cycles"], data["popSize"])
 # Initialise loop
 grid.displayGrid()
 grid.runCycle()
